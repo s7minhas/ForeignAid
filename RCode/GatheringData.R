f42a569832da04d2b5ceb5fe7926dbba37b3284b
Shahryar Minhas <hermes829@gmail.com>
2013-06-14 11:16:28 -0400

    starting syntax to combine data

---
+++ RCode/GatheringData.R
@@ -0,0 +1,48 @@
+# Loading workspace
+source('/Users/janus829/Desktop/Research/ButheProjects/ForeignAid/RCode/setup.R')
+
+## Loading data from WDI
+# need GDP per capita, Population, natural log of per capita aid, regional indicator
+
+
+### Other data
+# share of Christians mid 2000, from Barrett, Kurian, and Johnson
+
+
+## UNDP
+# HDI, share of pop: $1, $2, below local pov line, HPI, PQLI
+
+
+
+## WGI
+# control of corruption, rule of law
+
+
+
+## ICRG
+# corruption, rule of law, bureaucratic quality
+
+
+
+## Transparency index
+# corruption perceptions index
+
+
+
+## Burnside and Dollar's good policy index
+
+
+
+## Polity
+# regime type
+
+
+
+## Freedom House
+# regime type
+
+
+
+## Banks
+# political instability, conflict index
+
---
+++ RCode/setup.R
@@ -0,0 +1,26 @@
+# Clearing workspace
+rm(list=ls())
+
+# Setting working directory
+if(
+Sys.info()["user"]=="janus829"
+){pathMain="~/Dropbox/Research/ButheProjects/ForeignAid";
+  pathGraphics="~/Dropbox/Research/ButheProjects/ForeignAid/Graphics";
+  pathData="~/Dropbox/Research/ButheProjects/ForeignAid/Data";
+  pathCode="~/Desktop/Research/ButheProjects/ForeignAid/RCode";
+  pathFunctions="~/Desktop/Prog Notes/R Functions"}
+
+# Loading libraries and functions
+require(countrycode)
+setwd(pathFunctions)
+require(xlsx)
+require(gdata)
+require(reshape)
+require(plyr)
+require(WDI)
+
+require(lmer)
+
+# Setting seed
+set.seed(6886)
+setwd(pathMain)
