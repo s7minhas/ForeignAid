ed70928c0e39a0142a827f22eebdd265ae49da93
hermes829 <hermes829@gmail.com>
2014-06-13 18:34:52 -0400

    updates to aid data

--- RCode/Data/BuildingAidData.R
+++ RCode/Data/BuildingAidData.R
@@ -1,66 +1,33 @@
 if(Sys.info()["user"]=="janus829"){source("~/Desktop/Research/ForeignAid/RCode/setup.R")}
 
-setwd(paste(pathData, '/AidData', sep=''))
-fnames=list.files()
-aidList = list()
-for(ii in 1:length(fnames)){ aidList[[ii]] = read.csv(fnames[ii]) }
-names(aidList) = gsub('.csv','',fnames)
+################################################################
+setwd(paste0(pathData, '/AidData'))
+aidData=read.csv('aidDataAgg.csv')
+colnames(aidData)=c('Sender','Receiver','year','commitUSD09')
+################################################################
 
-# Cleaning aid dataset
-aidData=NULL
-for(ii in 1:length(aidList)){
-	temp=aidList[[ii]]; temp=temp[-1,
-		-which(names(temp) %in% c('Time.Period', 'X.1') ) ]
-	aid=melt( temp[,c(1,3:(ncol(temp)-1) ) ], id='X' )
-	names(aid)=c('Country', 'year', 'aid')
-	aid$year=num(substr(aid$year,2,5))
-	aid$aid=as.character(aid$aid)
-	aid$aid[aid$aid==".."]=NA
-	aid$aid=num(aid$aid)
-	aidData=rbind(aidData, cbind(Sender=names(aidList)[ii], aid)) }
+# Cleaning country names and adding ccodes
+senders=char(unique(aidData$Sender))
+senders=data.frame(cbind(cntry=senders,cnameS=cname(senders)))
+senders[is.na(senders[,2]),1] # check to make sure all NAs are IGOs
+senders$ccodeS=panel$ccode[match(senders$cnameS,panel$cname)]
+senders[is.na(senders[,3]),1] # check to make sure all NAs are IGOs
 
-# Cleaning sender names
-senderNames = data.frame(cbind(Sender=as.character(unique(aidData$Sender)), 
-	SenderClean=as.character(unique(aidData$Sender))))
-senderNames$SenderClean = as.character(senderNames$SenderClean)
+receivers=char(unique(aidData$Receiver))
+receivers=data.frame(cbind(cntry=receivers,cnameR=cname(receivers)))
+receivers[is.na(receivers[,2]),1] # check to make sure all NAs are IGOs/regions
+receivers$ccodeR=panel$ccode[match(receivers$cnameR,panel$cname)]
+receivers[is.na(receivers[,3]),1] # check to make sure all NAs are IGOs/regions
 
-senderNames$SenderClean[26] = 'New Zealand'
-senderNames$SenderClean[40] = 'United Arab Emirates'
+# Adding back into major dataframe
+aidData=merge(aidData,senders,by.x='Sender',by.y='cntry',all.x=T)
+aidData=merge(aidData,receivers,by.x='Receiver',by.y='cntry',all.x=T)
 
-senderNames$cname = cname(senderNames$SenderClean)
-aidData$cnameS = senderNames$cname[match(aidData$Sender, senderNames$Sender)]
-aidData$ccodeS = panel$ccode[match(aidData$cnameS, panel$cname)]
+# Dealing with NAs
+aidData=aidData[which(!is.na(aidData$cnameS)),] # gets rid of IGO sending cases
+aidData=aidData[which(!is.na(aidData$cnameR)),] # gets rid of region/group recipients
+aidData=aidData[which(!is.na(aidData$ccodeR)),] # gets rid of small countries
 
-# Cleaning up Receiver labels
-aidData$Country=char(aidData$Country)
-aidData$Country[aidData$Country=='Chinese Taipei']='Taiwan'
-aidData$cnameR = cname(aidData$Country)
-
-# Checking for  duplicate labels
-aidData$cnameRyr=paste(aidData$cnameR, aidData$year, sep='')
-table(aidData$cnameRyr)[table(aidData$cnameRyr)>42]
-
-# Adding ccode from panel dataset
-aidData$ccodeR = panel$ccode[match(aidData$cnameR, panel$cname)]
-# Dropping following small country cases
-#  [1] "Anguilla"               "Aruba"                  "Bermuda"               
-#  [4] "Cayman Islands"         "Cook Islands"           "Falkland Islands"      
-#  [7] "French Polynesia"       "Gibraltar"              "Hong Kong, China"      
-# [10] "Macao"                  "Mayotte"                "Montserrat"            
-# [13] "Netherlands Antilles"   "New Caledonia"          "Niue"                  
-# [16] "Northern Marianas"      "St. Helena"             "Tokelau"               
-# [19] "Turks & Caicos Islands" "Virgin Islands (UK)"    "Wallis & Futuna"       
-# [22] "West Bank & Gaza Strip"
-aidData = aidData[!is.na(aidData$ccodeR),]
-
-# Account for country existence
-# To do this I  drop all 
-# receiver country cases not in the panel dataset
-aidData$ccodeRyr=paste0(aidData$ccodeR, aidData$year)
-aidData=aidData[which(aidData$ccodeRyr %in% panel$ccodeYear),]
-
-# Subsetting to cases past 1970 due to missingness issues
-aidData=aidData[aidData$year>1970,]
 
 setwd(pathData)
 save(aidData, file='aidData.rda')
--- RCode/setup.R
+++ RCode/setup.R
@@ -4,10 +4,10 @@ rm(list=ls())
 # Setting working directory
 if(
 Sys.info()["user"]=="janus829"
-){pathMain="~/Dropbox/Research/ButheProjects/ForeignAid";
-  pathGraphics="~/Dropbox/Research/ButheProjects/ForeignAid/Graphics";
-  pathData="~/Dropbox/Research/ButheProjects/ForeignAid/Data";
-  pathCode="~/Desktop/Research/ButheProjects/ForeignAid/RCode";
+){pathMain="~/Google Drive/Research/ForeignAid";
+  pathGraphics="~/Google Drive/Research/ForeignAid/Graphics";
+  pathData="~/Google Drive/Research/ForeignAid/Data";
+  pathCode="~/Desktop/Research/ForeignAid/RCode";
   pathFunctions="~/Desktop/Prog Notes/R Functions"
   } else if  (Sys.info()["user"]=="cindycheng") {
   pathMain="~/Dropbox/ForeignAid";
@@ -18,29 +18,17 @@ Sys.info()["user"]=="janus829"
 
 
 # Loading libraries and functions
-library(foreign)
-library(WDI)
-library(cshapes)
-library(countrycode)
-library(plm)
-library(MASS)
-library(mvtnorm)
-library(reshape)
-library(gtools)
-library(ggplot2)
+require(foreign)
+require(cshapes)
+require(countrycode)
+
+require(reshape)
+require(gtools)
+require(ggplot2)
 theme_set(theme_bw())
-library(scales)
-library(gridExtra)
-library(plyr)
-library(doBy)
-library(tikzDevice)
-#setwd(pathFunctions)
-#source("rwish.R")
-#source("theme_border.R")
-library(lme4)
-library(arm)
-# source("hdr_2d.R")
-# source("scatterHist.R")
+
+require(doBy)
+require(tikzDevice)
 
 # Helper dataset
 setwd(pathData)
@@ -51,59 +39,43 @@ set.seed(6886)
 setwd(pathMain)
 
 # Helper functions
-trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+trim = function (x) gsub("^\\s+|\\s+$", "", x)
 
-substrRight <- function(x, n){
+substrRight = function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
 }
 
 # Log transformations for vars with negative values
-logNeg <- function(z){
-	x <- z[!is.na(z)]; y <- x
-	y[x>0] <- log(x[x>0]); y[x<0] <- -log(abs(x[x<0])); y[x==0] <- 0
-	z[!is.na(z)] <- y; z
+logNeg = function(z){
+	x = z[!is.na(z)]; y = x
+	y[x>0] = log(x[x>0]); y[x<0] = -log(abs(x[x<0])); y[x==0] = 0
+	z[!is.na(z)] = y; z
 }
 
 # Rescaling variables
-rescale <- function(x,new_max,new_min){
- xResc <- (new_max - new_min) / (max(x,na.rm=T) - min(x,na.rm=T))*(x - min(x,na.rm=T)) + new_min
+rescale = function(x,new_max,new_min){
+ xResc = (new_max - new_min) / (max(x,na.rm=T) - min(x,na.rm=T))*(x - min(x,na.rm=T)) + new_min
  xResc }
 
 # turn variables into numeric
-num <- function(x){ as.numeric(as.character(x)) }
+num = function(x){ as.numeric(as.character(x)) }
 char = function(x){as.character(x)}
 
 # Convert to cname
-cname <- function(x){
-	require(countrycode); x <- as.character(x)
-	y <- countrycode(x, 'country.name', 'country.name') }
-
-# Fx for Melting/Cleaning WB Data for Merge
-cleanAidData <- function(data, var, id, time){
-	mdata <- melt(data, id=id)
-	names(mdata)[3] <- var
-	mdata$year <-  num(mdata[,var])
-	mdata <- mdata[,c(id,time,var)]
-
-	# Setting standardized countryname for WB data
-	mdata$cname <- cname(mdata$Country)
-	mdata$cnameYear <- paste(mdata$cname, mdata$year, sep='')
-
-	# Adding in codes from panel
-	mdata$ccode <- panel$ccode[match(mdata$cname,panel$cname)]
-	mdata$cyear <- paste(mdata$ccode, mdata$year, sep='')
-	mdata }
+cname = function(x){
+	require(countrycode); x = as.character(x)
+	y = countrycode(x, 'country.name', 'country.name') }
 
 ### Fx for Melting/Cleaning WB Data for Merge
-cleanWbData <- function(data, variable){
-	var <- variable
-	mdata <- melt(data, id=c('Country.Name', 'Country.Code'))
-	names(mdata)[4] <- var
-	mdata$year <-  as.numeric(as.character(substring(mdata$variable,2)))
-	mdata <- mdata[,c(1,2,5,4)]
+cleanWbData = function(data, variable){
+	var = variable
+	mdata = melt(data, id=c('Country.Name', 'Country.Code'))
+	names(mdata)[4] = var
+	mdata$year =  as.numeric(as.character(substring(mdata$variable,2)))
+	mdata = mdata[,c(1,2,5,4)]
 
 	# Remove non-country observations and small islands/territories
-	drop <- c('Arab World', 'Caribbean small states', 
+	drop = c('Arab World', 'Caribbean small states', 
 		'East Asia & Pacific (all income levels)', 
 		'East Asia & Pacific (developing only)', 'Euro area', 
 		'Europe & Central Asia (all income levels)', 
@@ -137,16 +109,16 @@ cleanWbData <- function(data, variable){
 		 "Puerto Rico",               "Sint Maarten (Dutch part)",
 		 "St. Martin (French part)",  "Turks and Caicos Islands", 
 		 "Virgin Islands (U.S.)",     "West Bank and Gaza")
-	mdata <- mdata[which(!mdata$Country.Name %in% drop),]
+	mdata = mdata[which(!mdata$Country.Name %in% drop),]
 
 	# Setting standardized countryname for WB data
-	mdata$Country.Name <- as.character(mdata$Country.Name)
-	mdata$Country.Name[mdata$Country.Name=='Korea, Dem. Rep.'] <- 'North Korea' 
-	mdata$Country.Name[mdata$Country.Name=='Korea, Rep.'] <- 'South Korea' 
-	mdata$cname <- cname(mdata$Country.Name)
-	mdata$cnameYear <- paste(mdata$cname, mdata$year, sep='')
+	mdata$Country.Name = as.character(mdata$Country.Name)
+	mdata$Country.Name[mdata$Country.Name=='Korea, Dem. Rep.'] = 'North Korea' 
+	mdata$Country.Name[mdata$Country.Name=='Korea, Rep.'] = 'South Korea' 
+	mdata$cname = cname(mdata$Country.Name)
+	mdata$cnameYear = paste(mdata$cname, mdata$year, sep='')
 	
 	# Adding in codes from panel
-	mdata$ccode <- panel$ccode[match(mdata$cname,panel$cname)]
-	mdata$cyear <- paste(mdata$ccode, mdata$year, sep='')
+	mdata$ccode = panel$ccode[match(mdata$cname,panel$cname)]
+	mdata$cyear = paste(mdata$ccode, mdata$year, sep='')
 	mdata }	
