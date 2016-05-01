47db751621b8d8e2f430be91ea5705e064c3b99a
s7minhas <s7minhas@gmail.com>
2016-05-01 02:34:39 -0700

    move save outsde of parallel loop

--- RCode/Analysis/2_reModels.R
+++ RCode/Analysis/2_reModels.R
@@ -45,12 +45,12 @@ runModelParallel = function(cores=5, dataList, trainLogic, trainEnd=2002, modTyp
 	print(paste0('Running model: ', Reduce(paste, deparse(modForm))))
 	print(paste0('Sainvg to: ', modName))
 	cl=makeCluster(cores) ; registerDoParallel(cl)
-	foreach(ii=1:length(dataList), .packages=c('glmmADMB')) %dopar% {
+	mods = foreach(ii=1:length(dataList), .packages=c('glmmADMB')) %dopar% {
 		regData = dataList[[ii]] # Subset to relevant data
 		if(trainLogic){regData$year=num(regData$year); regData=regData[regData$year<trainEnd,] ; regData$year=factor(regData$year, levels=sort(unique(regData$year))) }	
 		m=glmmadmb(modForm, data=regData, zeroInflation=zeroInfLogic, family=modType, extra.args="-ndi 100000") # Run
-		save(m, file=paste0(pathResults, '/', modName)) # Save output
-	} ; stopCluster(cl)	
+	} ; stopCluster(cl)
+	save(mods, file=paste0(pathResults, '/', modName)) # Save output	
 }
 
 # Model 1
