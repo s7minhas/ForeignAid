6de2bc7328938b00290d473414e3f16b071d1c73
cindyyawencheng <ccheng623@gmail.com>
2018-02-21 14:06:54 +0100

    loop runTnsr

--- RCode/stratInterest/1_runTnsr.R
+++ RCode/stratInterest/1_runTnsr.R
@@ -1,15 +1,13 @@
 if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
 	source('~/Research/ForeignAid/RCode/setup.R') }
+loadPkg('rTensor')
 
 ############################
 # Load amen data
-load( paste0(pathTnsr,'amenData_all_rescaled.rda') )
-load( paste0(pathTnsr,'amenData_all_stdz.rda') )
-# load( paste0(pathTnsr,'amenData_all.rda') )
+#load( paste0(pathTnsr,'amenData_all_rescaled.rda') )
+# load( paste0(pathTnsr,'amenData_all_stdz.rda') )
+load( paste0(pathTnsr,'amenData_all.rda') )
 dimnames(amData[[1]])[[3]]
-
-
-head(amData[[1]][,,8])
 
 ############################
 
@@ -18,44 +16,60 @@ head(amData[[1]][,,8])
 dir.create(paste0(pathTnsr, 'tnsrSpace/'), showWarnings=FALSE)
 
 # Subset to relev vars
- inclVars = c('agree3un','totAllyCnt','igo')
-# inclVars = c('agree3un','allyWt','igo')
-# inclVars = c('agree3un','allyWt','igoWt')
-# inclVars = c('agree3un','totAllyCnt','igoWt')
-# inclVars = c('idealPtun','totAllyCnt','igo')
-# inclVars = c('idealPtun','allyWt','igo')
-# inclVars = c('idealPtun','allyWt','igoWt')
-# inclVars = c('idealPtun','totAllyCnt','igoWt')
-# inclVars = c('idealPtun','defense','igo')
-# inclVars = c('idealPtun','defEntSum','igo')
-# inclVars = c('idealPtun','defEnt','igo')
 
 
-amDataRaw = lapply(amData, function(x){
+testVars = list( c('agree3un','totAllyCnt','igo'),
+	c('agree3un','allyWt','igo'),
+	c('agree3un','allyWt','igoWt'),
+ 	c('agree3un','totAllyCnt','igoWt'),
+ 	c('idealPtun','totAllyCnt','igo'),
+ 	c('idealPtun','allyWt','igo'),
+ 	c('idealPtun','allyWt','igoWt'),
+ 	c('idealPtun','totAllyCnt','igoWt'),
+ 	c('idealPtun','defense','igo'),
+ 	c('idealPtun','defEntSum','igo'),
+ 	c('idealPtun','defEntSum','igoWt'),
+ 	c('idealPtun','defEnt','igo'),
+ 	c('idealPtun','defEnt','igoWt'),
+ 	c('agree3un','defEntSum','igo'),
+ 	c('agree3un','defEntSum','igoWt'),
+ 	c('agree3un','defEnt','igo'),
+ 	c('agree3un','defEnt','igoWt'))
+ 
+
+results = list()
+for ( ii in 1:length(testVars)){
+	inclVars = testVars[[ii]]
+	amDataRaw = lapply(amData, function(x){
 	x = x[,,inclVars]
 	return(x)
 	})
 
-#
-#loadPkg('rTensor')
-amDataSl = amDataRaw[[1]]
-amDataSl[is.na(amDataSl)] = 0
-amDataSlTnsr = as.tensor(amDataSl)
-amDataSlTckr <- tucker(amDataSlTnsr, ranks=c(20,20,3))
+	amDataSl = amDataRaw[[1]]
+	amDataSl[is.na(amDataSl)] = 0
+	amDataSlTnsr = as.tensor(amDataSl)
+	amDataSlTckr <- tucker(amDataSlTnsr, ranks=c(143,143,1))
+
+ 	results[[ii]] = c(amDataSlTckr$norm_percent, inclVars)
+
+}
+
+rawTnsr = do.call(rbind, results)
+stdTnsr = do.call(rbind, results)
+rescaleTnsr = do.call(rbind, results)
+
+rawTnsr 
+# check performance
+
+# amDataSlApprox <- ttl(amDataSlTckr$Z, amDataSlTckr$U, 1:3)
+# fnorm(amDataSlTnsr - amDataSlApprox) / fnorm(amDataSlTnsr)
 
 
-amDataSlTckr_hosvd <- tucker(amDataSlTnsr, ranks=c(20,20,3))
-hosvd_prod = ttl(amDataSlTckr_hosvd$Z, amDataSlTckr_hosvd$U, 1:3)
-
- <- tnsr - HOSVD_prod
-table(abs(error@data) < 1e-12)
- 
- 
-
-amDataSlApprox <- ttl(amDataSlTckr$Z, amDataSlTckr$U, 1:3)
-fnorm(amDataSlTnsr - amDataSlApprox) / fnorm(amDataSlTnsr)
-
-amDataSlTckr$norm_percent
+inclVars = c('agree3un','defEntSum','igo')
+amDataFinal = lapply(amData, function(x){
+	x = x[,,inclVars]
+	return(x)
+	})
 
 # Parallelize run for every year
 cl = makeCluster(6)
@@ -63,18 +77,24 @@ registerDoParallel(cl)
 yrs = names(amData)
 res=foreach(yr = yrs, .packages=c("rTensor")) %dopar% {
 	# Run tucker decomp
-	amDataSl = amDataRaw[[yr]]
+	amDataSl = amDataFinal[[yr]]
 	amDataSl[is.na(amDataSl)] = 0
 	amDataSlTnsr = as.tensor(amDataSl)
-	amDataSlTckr <- tucker(amDataSlTnsr, ranks=c(20,20,3))
+	amDataSlTckr <- tucker(amDataSlTnsr, ranks=c(143,143,1))
 }
 
-
-
-lapply(res, function(x) x$norm_percent)
 # Free my clusters
 stopCluster(cl)
+
+# check performance
+lapply(res, function(x) x$norm_percent)
+
+# save file
+save(res, file = paste0(pathTnsr, 'tnsrSpace/tnsr_Ideal_DefEnt_igoWt.rda'))
 ############################
+
+
+
 
 hamming distances
 if we want to get a lower respresentation, we can use hamming distances, to calaculate a euclidean distance 
