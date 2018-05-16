8fde1bc8b772afed675c977afee270c770092654
cindyyawencheng <ccheng623@gmail.com>
2018-05-16 17:31:38 +0200

    further disaggregate aid into development, humanitarian and civil society

--- RCode/Data/1_buildAIdDataDisagg.R
+++ RCode/Data/1_buildAIdDataDisagg.R
@@ -3,16 +3,6 @@ if(Sys.info()['user']=='cindycheng'){ source('~/Documents/Papers/ForeignAid/RCod
 library(dplyr)
 ################################################################
 setwd(paste0(pathData, '/components/AidDataCore_ResearchRelease_Level1_v3'))
-
-load(paste0(pathData, '/iData_v2.rda'))
-
-senders = iData[[1]]$ccodeS %>% unique() %>% as.character() %>% as.numeric()
-panel$CNTRY_NAME[match(senders, panel$ccode)] %>% unique()
-
-
-recipients = iData[[1]]$ccodeR %>% unique() %>% as.character() %>% as.numeric()
-panel$CNTRY_NAME[match(recipients, panel$ccode)] %>% unique() %>% sort()
-
 aidDataRaw= read.csv('AidDataCoreDonorRecipientYearPurpose_ResearchRelease_Level1_v3.1.csv', stringsAsFactors = FALSE)
 
 # Get aggregated data, note you double checked and this aggregation 
@@ -28,23 +18,66 @@ aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code == 70000)] = '
 aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(72000, 72010, 72020, 72030, 72040, 72050))] = 'emergencyResponse'
 aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(73010))] = 'reconstructionRelief'
 aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(74010))] = 'disasterPreventionRelief'
-    
  
-aidDataHumanitarian = aidDataRaw[-which(is.na(aidDataRaw$purposeNameAgg)),]
-aidDataHumanitarianAgg = aidDataHumanitarian %>% group_by(donor, recipient, year, purposeNameAgg) %>% summarise(commitment_amount_usd_constant_sum = sum(commitment_amount_usd_constant_sum)) %>% data.frame()
+# Get democracy/civil society data
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(15000, 15100, 15105, 15150))] = 'govCivilSociety'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(42010))] = 'womenSupport'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(92000, 92005, 92010, 92020, 92030))] = 'ngoGovSupport'
+
+
+# Get development data
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(11000, 11100, 11105, 11110, 11120, 11130, 11182, 11220, 11230, 11240, 11320, 11330, 11420, 11430))] = 'education'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(12000, 12005, 12100, 12105, 12110, 12181, 12182, 12191, 12220, 12230,12240,12250, 12261, 12281, 13000, 13005, 13010, 13020, 13030, 13040, 13081))] = 'health'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(14000, 14005, 14010, 14015, 14020, 14030, 14040, 14050, 14081, 14082))] = 'waterSanitation'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(16020, 16030, 16050, 16081))] = 'otherSocialInfrastructureAndServices'
+
+
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(21005, 21010, 21020, 21030, 21040, 21050, 21061, 21081, # transportation and storage
+																		23000, 23005, 23010, 23020, 23030, 23040, 23050, 23055, 23081, 23082, # energy
+																		24000, 24005, 24010, 24020, 24030, 24040, 24081, 25010, 25020,25081# banking
+																												))] = 'economicInfrastructureAndServices'
+
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(31000, 31100, 31105,31110,31120,31130, 31140, 31150, 31181, 31182, 31191, 31205, 31210, 31220, 31281, 31282, 31291, 31300, 31305, 31310, 31320, 31330, 31381, 31382, 31391))] = 'agricultureForestyFishing'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(32000, 32105, 32110, 32120, 32130, 32140, 32181, 32182, 32191, 32200, 32205, 32210, 32220, 32281, 32310))] = 'industryMiningConstruction'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(41000, 41005, 41010, 41020, 41030, 41040, 41050, 41081, 41082))] = 'environmentalProtection'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(33210, 43030, 43040, 43050, 43050, 43081,  43082))] = 'otherDevelopmentAid'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(52010))] = 'foodAid'
+aidDataRaw$purposeNameAgg[which(aidDataRaw$coalesced_purpose_code %in% c(60010, 60020, 60030, 60040))] = 'debtRelief'
+
+
+
+# not included ; the reasons these sectors were not included was that their ability to purely promote development seemed muddled
+# Economic and development planning/policy; legal and judicial development; government administration
+# `conflict prevention and resolution, peace and security`; 
+# `other social infrastructure and services': 16010, social/welfare services
+# communications: includes stuff like funding for radio, television
+# other commodity assistnace: import support, export support
+# trade policy and regulations
+# refugees
+# unallocated/unsepcified
+# administrative costs of donors
+ 
+
+
+aidDataSectors= aidDataRaw[-which(is.na(aidDataRaw$purposeNameAgg)),]
+aidDataSectorsAgg = aidDataSectors %>% group_by(donor, recipient, year, purposeNameAgg) %>% summarise(commitment_amount_usd_constant_sum = sum(commitment_amount_usd_constant_sum)) %>% data.frame()
 
 
  
-aidDataHumanitarianAgg = reshape(aidDataHumanitarianAgg,
+aidDataSectorsAgg = reshape(aidDataSectorsAgg,
 	timevar = "purposeNameAgg",
 	idvar = c('donor', 'recipient', 'year'),
 	direction = 'wide')
-names(aidDataHumanitarianAgg) = gsub('commitment_amount_usd_constant_sum.', '', names(aidDataHumanitarianAgg))
-aidDataHumanitarianAgg$humanitarianTotal = rowSums(aidDataHumanitarianAgg[, c('emergencyResponse', 'humanitarianAid', 'reconstructionRelief', 'disasterPreventionRelief')], na.rm = TRUE)
-
+names(aidDataSectorsAgg) = gsub('commitment_amount_usd_constant_sum.', '', names(aidDataSectorsAgg))
+aidDataSectorsAgg$humanitarianTotal = rowSums(aidDataSectorsAgg[, c('emergencyResponse', 'humanitarianAid', 'reconstructionRelief', 'disasterPreventionRelief')], na.rm = TRUE)
+aidDataSectorsAgg$civSocietyTotal = rowSums(aidDataSectorsAgg[, c('govCivilSociety', 'womenSupport',  'ngoGovSupport')], na.rm = TRUE)
+aidDataSectorsAgg$developTotal = rowSums(aidDataSectorsAgg[, c('education', 'health', 'waterSanitation', 'otherSocialInfrastructureAndServices', 'economicInfrastructureAndServices', 'industryMiningConstruction', 'environmentalProtection', 'otherDevelopmentAid', 'debtRelief')], na.rm = TRUE)
+ 
+  
+ 
  
 # combine 
-aidData = merge(aidDataAgg, aidDataHumanitarianAgg, by = c('donor', 'recipient', 'year'), all.x = TRUE)
+aidData = merge(aidDataAgg, aidDataSectorsAgg, by = c('donor', 'recipient', 'year'), all.x = TRUE)
 aidData[is.na(aidData)] = 0
 aidData$notHumanitarianTotal = aidData$commitment_amount_usd_constant_sum - aidData$humanitarianTotal
 ################################################################
@@ -103,24 +136,24 @@ aidData = aidData[-which(aidData$year == 9999),]
 aidMats=DyadBuild(variable='commitment_amount_usd_constant_sum', dyadData=aidData,
 	cntry1='ccodeS', cntry2='ccodeR', time='year',
 	pd=1970:2010, panel=panel, directed=TRUE)
-aidMatsEmergency = DyadBuild(variable='emergencyResponse', dyadData=aidData,
-	cntry1='ccodeS', cntry2='ccodeR', time='year',
-	pd=1970:2010, panel=panel, directed=TRUE) 
-aidMatsHumanitarian = DyadBuild(variable='humanitarianAid', dyadData=aidData,
-	cntry1='ccodeS', cntry2='ccodeR', time='year',
-	pd=1970:2010, panel=panel, directed=TRUE) 
-aidMatsReconstruction = DyadBuild(variable='reconstructionRelief', dyadData=aidData,
-	cntry1='ccodeS', cntry2='ccodeR', time='year',
-	pd=1970:2010, panel=panel, directed=TRUE) 
-aidMatsDisaster = DyadBuild(variable='disasterPreventionRelief', dyadData=aidData,
-	cntry1='ccodeS', cntry2='ccodeR', time='year',
-	pd=1970:2010, panel=panel, directed=TRUE) 
-aidMatsHumanitarianTotal = DyadBuild(variable='humanitarianTotal', dyadData=aidData,
-	cntry1='ccodeS', cntry2='ccodeR', time='year',
-	pd=1970:2010, panel=panel, directed=TRUE) 
-aidMatsNotHumanitarianTotal = DyadBuild(variable='notHumanitarianTotal', dyadData=aidData,
-	cntry1='ccodeS', cntry2='ccodeR', time='year',
-	pd=1970:2010, panel=panel, directed=TRUE) 
+# aidMatsEmergency = DyadBuild(variable='emergencyResponse', dyadData=aidData,
+# 	cntry1='ccodeS', cntry2='ccodeR', time='year',
+# 	pd=1970:2010, panel=panel, directed=TRUE) 
+# aidMatsHumanitarian = DyadBuild(variable='humanitarianAid', dyadData=aidData,
+# 	cntry1='ccodeS', cntry2='ccodeR', time='year',
+# 	pd=1970:2010, panel=panel, directed=TRUE) 
+# aidMatsReconstruction = DyadBuild(variable='reconstructionRelief', dyadData=aidData,
+# 	cntry1='ccodeS', cntry2='ccodeR', time='year',
+# 	pd=1970:2010, panel=panel, directed=TRUE) 
+# aidMatsDisaster = DyadBuild(variable='disasterPreventionRelief', dyadData=aidData,
+# 	cntry1='ccodeS', cntry2='ccodeR', time='year',
+# 	pd=1970:2010, panel=panel, directed=TRUE) 
+# aidMatsHumanitarianTotal = DyadBuild(variable='humanitarianTotal', dyadData=aidData,
+# 	cntry1='ccodeS', cntry2='ccodeR', time='year',
+# 	pd=1970:2010, panel=panel, directed=TRUE) 
+# aidMatsNotHumanitarianTotal = DyadBuild(variable='notHumanitarianTotal', dyadData=aidData,
+# 	cntry1='ccodeS', cntry2='ccodeR', time='year',
+# 	pd=1970:2010, panel=panel, directed=TRUE) 
 ################################################################
 
 ###############################################################
@@ -138,6 +171,8 @@ rCntries = unique(aidData$cnameR)
 
 ################################################################
 setwd(pathData)
-save(aidData, aidMats, aidMatsEmergency, aidMatsHumanitarian, aidMatsReconstruction,
-	aidMatsDisaster, aidMatsHumanitarianTotal, aidMatsNotHumanitarianTotal, file='aidDataDisagg.rda')
+save(aidData, 
+	#aidMats, aidMatsEmergency, aidMatsHumanitarian, aidMatsReconstruction,
+	#aidMatsDisaster, aidMatsHumanitarianTotal, aidMatsNotHumanitarianTotal, 
+	file='aidDataDisagg.rda')
 ################################################################
