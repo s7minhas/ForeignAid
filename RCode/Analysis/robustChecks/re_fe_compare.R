258204315d3477d14d73b846e6ead741478f7161
s7minhas <s7.minhas@gmail.com>
2019-03-22 08:40:03 -0400

    coef plot for fe re compare

--- RCode/Analysis/robustChecks/re_fe_compare.R
+++ RCode/Analysis/robustChecks/re_fe_compare.R
@@ -110,3 +110,87 @@ feMods = list(
 reMods
 feMods
 ################################################################
+
+################################################################
+# vars
+cntrlVars=c(
+  'Lno_disasters', 'colony', 'Lpolity2',
+  'LlnGdpCap', 'LlifeExpect', 'Lcivwar' )
+cntrlVarNames = c(
+  'No. Disasters$_{r,t-1}$',    
+  'Former Colony$_{sr,t-1}$',
+  'Polity$_{r,t-1}$',
+  'Log(GDP per capita)$_{r,t-1}$',
+  'Life Expectancy$_{r,t-1}$',
+  'Civil War$_{r,t-1}$'
+  )
+varsNoInt=c('LstratMu', cntrlVars)
+varNamesNoInt = c('Strategic Distance$_{sr,t-1}$', cntrlVarNames)
+varsInt=c(
+  'LstratMu', cntrlVars[1], 'LstratMu:Lno_disasters', cntrlVars[-1])
+varNamesInt = c('Strategic Distance$_{sr,t-1}$', cntrlVarNames[1],
+  'Strategic Distance$_{sr,t-1}$\n $\\times$ No. Disasters$_{r,t-1}$', 
+  cntrlVarNames[-1])
+
+# 
+coefp_colors = c("Positive"=rgb(54, 144, 192, maxColorValue=255), 
+  "Negative"= rgb(222, 45, 38, maxColorValue=255),
+  "Positive at 90"=rgb(158, 202, 225, maxColorValue=255), 
+  "Negative at 90"= rgb(252, 146, 114, maxColorValue=255),
+  "Insig" = rgb(150, 150, 150, maxColorValue=255))
+
+summarizeMods = function(mods, dirtyVars, cleanVars){
+  modSumm = lapply(1:length(mods), function(i){
+    mod = mods[[i]]; summ = rubinCoef(mod)
+    summ$dv = names(mods)[i]
+    summ$up95 = with(summ, beta + qnorm(.975)*se)
+    summ$lo95 = with(summ, beta - qnorm(.975)*se)
+    summ$up90 = with(summ, beta + qnorm(.95)*se)
+    summ$lo90 = with(summ, beta - qnorm(.95)*se)
+    summ = summ[summ$var!='(Intercept)',]    
+    summ$varClean = cleanVars[match(summ$var, dirtyVars)]
+    summ$dvClean = dvNames[match(summ$dv, dvs)]
+    summ$sig = NA
+    summ$sig[summ$lo90 > 0 & summ$lo95 < 0] = "Positive at 90"
+    summ$sig[summ$lo95 > 0] = "Positive"
+    summ$sig[summ$up90 < 0 & summ$up95 > 0] = "Negative at 90"
+    summ$sig[summ$up95 < 0] = "Negative"
+    summ$sig[summ$lo90 < 0 & summ$up90 > 0] = "Insig"
+    return(summ) }) %>% do.call('rbind', .)
+  modSumm$varClean = factor(modSumm$varClean, levels=rev(cleanVars))
+  modSumm$dvClean = factor(modSumm$dvClean, levels=dvNames[c(1,3,2)])
+  return(modSumm) }
+
+#
+intModSumm = summarizeMods(stratMuIntMods, varsInt, varNamesInt)
+################################################################
+
+################################################################
+# Model results
+plotRes = function(modSumm){
+  # fix some labels
+  xlabels = TeX(char(modSumm$varClean))
+  xlabels[char(modSumm$varClean)==varNamesInt[3]] = expression( atop(
+      'Strategic Distance' ['sr,t-1'],
+      'x No. Disasters' ['r,t-1'] ) )
+
+  # viz
+  ggplot(modSumm, aes(x=varClean, y=beta, color=sig)) +
+    geom_hline(aes(yintercept=0), linetype='dashed', color='grey40') + 
+    geom_point() +
+    geom_linerange(aes(ymin=lo95,ymax=up95), size=.3) +
+    geom_linerange(aes(ymin=lo90,ymax=up90), size=1) +
+    scale_color_manual(values=coefp_colors) +
+    scale_x_discrete('',labels=xlabels) +    
+    coord_flip() +
+    facet_wrap(~dvClean, ncol=4, scales='free_x') +
+    labs( y='' ) +
+    theme(
+      axis.ticks = element_blank(), 
+      panel.border=element_blank(),
+      legend.position='none' ) }
+
+intGG = plotRes(intModSumm)
+ggsave(intGG, 
+  file=paste0(pathGraphics, '/intCoef.pdf'), width=8, height=6)
+################################################################
