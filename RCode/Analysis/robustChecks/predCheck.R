af26fb8c3bd42d7ef85fac2c1d26b3b7a6fe5b9b
s7minhas <s7.minhas@gmail.com>
2019-03-25 17:36:06 -0400

    in sample fit comparison

---
+++ RCode/Analysis/robustChecks/predCheck.R
@@ -0,0 +1,108 @@
+if(Sys.info()['user']=='s7m' | Sys.info()['user']=='janus829'){
+  source('~/Research/ForeignAid/RCode/setup.R') }
+if(Sys.info()['user']=='cindycheng'){
+  source('~/Documents/Papers/ForeignAid/RCode/setup.R') }
+
+#
+loadPkg(c('lme4'))
+################################################################
+
+################################################################
+# add extra vars for reviewers
+## the .rda below is created in latVarUncertainty.R
+load( paste0(pathData, 'iData_for_r3.rda') )
+
+# results similar across imputed datasets
+iData = iData[[length(iData)]]
+################################################################
+
+################################################################
+# specs
+dvs = c(
+  'humanitarianTotal',
+  'developTotal',
+  'civSocietyTotal'
+  )
+dvNames = paste0(
+  c('Humanitarian', 'Development', 'Civil Society'), ' Aid')
+baseSpec = paste(
+  c(
+    'LstratMu', 'Lno_disasters', 
+    'LstratMu * Lno_disasters', 'colony', 
+    'Lpolity2', 'LlnGdpCap', 'LlifeExpect',
+    'Lcivwar'
+  ), collapse=' + ' )
+reStruc = '+ (1|id) + (1|year)'
+
+# set up formulas
+reModSpecs = lapply(dvs, function(y){
+  formula(paste0(y, '~', baseSpec, reStruc)) })
+
+# spec using ally variable
+altSpec = paste(
+  c(
+    'LallyWt', 'Lno_disasters', 
+    'LallyWt * Lno_disasters', 'colony', 
+    'Lpolity2', 'LlnGdpCap', 'LlifeExpect',
+    'Lcivwar'
+  ), collapse=' + ' )
+# set up formulas
+reModSpecsAlt = lapply(dvs, function(y){
+  formula(paste0(y, '~', altSpec, reStruc)) })
+
+# combine specs
+allSpecs = list(reModSpecs, reModSpecsAlt)
+################################################################
+
+################################################################
+# run models
+# if(!file.exists(
+#   paste0(pathResults, '/reMods_latVarUncert.rda')
+#   )){
+
+# out of sample set up for comparison
+nFolds = 30
+iData$fold = sample(1:nFolds, nrow(iData), replace=TRUE)
+
+for(f in 1:nFolds){
+  
+  # divide into train and test
+  train = iData[iData$fold != f,]
+  test = iData[iData$fold == f,]
+
+  # run train models
+  mods = 
+
+}
+
+  ## humanitarian model
+  humModRE= lmer(reModSpecs[[1]], data=iData)
+  humRMSE = sqrt(mean( (predict(humModRE)-iData$humanitarianTotal)^2 ))
+  humAltModRE= lmer(reModSpecsAlt[[1]], data=iData)
+  humAltRMSE = sqrt(mean( (predict(humAltModRE)-iData$humanitarianTotal)^2 ))
+
+  ## dev model
+  devModRE = lmer(reModSpecs[[2]], data=iData)
+  devRMSE = sqrt(mean( (predict(devModRE)-iData$developTotal)^2 ))
+  devAltModRE = lmer(reModSpecsAlt[[2]], data=iData)
+  devAltRMSE = sqrt(mean( (predict(devAltModRE)-iData$developTotal)^2 ))
+
+  ## civil society model
+  civModRE = lmer(reModSpecs[[3]], data=iData)
+  civRMSE = sqrt(mean( (predict(civModRE)-iData$civSocietyTotal)^2 ))
+  civAltModRE = lmer(reModSpecsAlt[[3]], data=iData)
+  civAltRMSE = sqrt(mean( (predict(civAltModRE)-iData$civSocietyTotal)^2 ))
+
+  # org rmse
+  rmseMat = matrix(NA,nrow=3,ncol=2,dimnames=list(dvs, c('Original','Alternative')))
+  rmseMat[,1] = c(humRMSE,devRMSE,civRMSE)
+  rmseMat[,2] = c(humAltRMSE,devAltRMSE,civAltRMSE)
+  rmseMat
+
+  # save(reMods, 
+  #   file=paste0(pathResults, '/reMods_latVarUncert.rda')
+  #   )
+# } else {
+#   load(paste0(pathResults, '/reMods_latVarUncert.rda'))
+# }
+################################################################
