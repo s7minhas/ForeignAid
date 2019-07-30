78559d52315a2c3b3f9c7e20342951ed4aeeedc6
s7minhas <s7.minhas@gmail.com>
2019-07-29 23:49:38 -0400

    lag struct robustness check when including lagged dv

--- RCode/Analysis/robustChecks/aidDependence.R
+++ RCode/Analysis/robustChecks/aidDependence.R
@@ -416,7 +416,115 @@ stratMuIntMods_5 = foreach(
     mod = lmer(spec, data=regData_5)
     return(mod) } ; stopCluster(cl)
 names(stratMuIntMods_5) = dvs
+
+# org results by dv
+humMods = list(
+  stratMuIntMods$humanitarianTotal,
+  stratMuIntMods_3$humanitarianTotal,
+  stratMuIntMods_5$humanitarianTotal
+  )
+
+devMods = list(
+  stratMuIntMods$developTotal,
+  stratMuIntMods_3$developTotal,
+  stratMuIntMods_5$developTotal
+  )
+
+civMods = list(
+  stratMuIntMods$civSocietyTotal,
+  stratMuIntMods_3$civSocietyTotal,
+  stratMuIntMods_5$civSocietyTotal
+  )
+
+mods = list(humMods, devMods, civMods)
+names(mods) = dvs
 ################################################################
 
+################################################################
+# calc substantive effect
+simPlots = list()
+for(i in 1:length(dvs)){
+  stratMuIntMods = mods[[i]]
+  names(stratMuIntMods) = paste0('Lag ', c(1,3,5))
 
-#########################################################
+  simResults = lapply(1:length(stratMuIntMods), function(j){
+    mod = stratMuIntMods[[j]] ; var = names(fixef(mod))[3]
+    disVar = names(fixef(mod))[4]
+
+    # Create scenario matrix
+    stratQts = quantile(regData[,var], probs=c(.05,.95), na.rm=TRUE)
+    stratRange=with(data=regData, seq(stratQts[1], stratQts[2], .01) )
+    disRange=seq( min(regData[,disVar],na.rm=TRUE), 4, 2 ) 
+    scen = with(data=regData, 
+      expand.grid(
+        1, 
+        median(get(paste0('L',dvs[i])), na.rm=TRUE),
+        stratRange, disRange, 
+        median(colony,na.rm=TRUE), median(Lpolity2,na.rm=TRUE), 
+        median(LlnGdpCap,na.rm=TRUE), 
+        median(LlifeExpect,na.rm=TRUE),median(Lcivwar,na.rm=TRUE)
+        ) )
+
+    # Add interaction term
+    scen = cbind( scen, scen[,3]*scen[,4] )
+    colnames(scen) = names(fixef(mod))
+    scen = data.matrix(scen)
+    pred = scen %*% mod@beta
+    draws = mvrnorm(10000, mod@beta, vcov(mod))
+    sysUncert = scen %*% t(draws)
+    sysInts95 = t(apply(sysUncert, 1, function(x){
+      quantile(x, c(0.025, 0.975), na.rm=TRUE) }))
+    sysInts90 = t(apply(sysUncert, 1, function(x){
+      quantile(x, c(0.05, 0.95), na.rm=TRUE) }))
+
+    # Combine for plotting
+    ggData=data.frame(
+        cbind(pred, sysInts95, sysInts90, 
+          scen[,var], scen[,disVar])
+        )
+    names(ggData)=c('fit', 'sysLo95', 'sysHi95', 
+      'sysLo90', 'sysHi90', var, disVar)
+    names(ggData)[6:7] = c('LstratMu','Lno_disasters')
+
+    # Plot rel at various cuts of disasters
+    disRange=with(data=regData, seq(
+      min(Lno_disasters), 4, 2) )
+    ggDataSmall = ggData[which(ggData$Lno_disasters %in% disRange),]
+
+    # change facet labels
+    ggDataSmall$Lno_disasters = paste(ggDataSmall$Lno_disasters, 'Disasters$_{r}$')
+
+    #
+    ggDataSmall$lagID = names(stratMuIntMods)[j]
+    return(ggDataSmall) })
+
+  # viz
+  ggData = do.call('rbind', simResults)
+  facet_labeller = function(string){ TeX(string) }
+  modTitle = dvNames[i]
+  tmp=ggplot(ggData, aes(x=LstratMu, y=fit)) +
+    geom_line() +
+    geom_ribbon(aes(ymin=sysLo90, ymax=sysHi90), alpha=.6) +
+    geom_ribbon(aes(ymin=sysLo95, ymax=sysHi95), alpha=.4) +
+    facet_grid(Lno_disasters~lagID, labeller=as_labeller(facet_labeller, default = label_parsed)) +
+    labs(
+      x=TeX('Strategic Distance$_{sr}$'),
+      y=TeX("Log(Aid)$_{t}$"),
+      title=modTitle
+      ) +
+    theme(
+      axis.ticks=element_blank(), 
+      panel.border = element_blank() )
+  simPlots[[i]] = tmp
+}
+
+ggsave(simPlots[[1]], 
+  file=paste0(pathGraphics, '/simComboPlot_lag_hAid_lagDV.pdf'), 
+  width=7, height=4)
+ggsave(simPlots[[2]], 
+  file=paste0(pathGraphics, '/simComboPlot_lag_dAid_lagDV.pdf'), 
+  width=7, height=4)
+ggsave(simPlots[[3]], 
+  file=paste0(pathGraphics, '/simComboPlot_lag_cAid_lagDV.pdf'), 
+  width=7, height=4)
+################################################################
