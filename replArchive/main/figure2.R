4ec5bd2d4ef389f03f1b2eff2b748facfd08c491
s7minhas <s7.minhas@gmail.com>
2020-05-06 21:20:53 -0400

    modify cols in lat space

--- replArchive/main/figure2.R
+++ replArchive/main/figure2.R
@@ -1,3 +1,4 @@
+setwd('C:/Users/herme/Research/foreignAid/replArchive/main/')
 source('setup.R')
 ################################################################
 
@@ -11,41 +12,41 @@ latSpaceGen = function(var,bw=TRUE){
   # read in gen out for dims
   outName = paste0("intake/",var,"_2005_OUT")
   OUT<-read.table(outName,header=T)             #read in output
-  
+
   # pull in lat positions
   zName = paste0("intake/",var,"_2005_Z")
   Z<-read.table(zName)
-  
+
   # proc transformation
   proc.rr<-function(Y,X){
     k<-dim(X)[2]
     A<-t(Y)%*%(  X%*%t(X)  )%*%Y
     eA<-eigen(A,symmetric=T)
     Ahalf<-eA$vec[,1:k]%*%diag(sqrt(eA$val[1:k]),nrow=k)%*%t(eA$vec[,1:k])
-    t(t(X)%*%Y%*%solve(Ahalf)%*%t(Y)) }  
-  
+    t(t(X)%*%Y%*%solve(Ahalf)%*%t(Y)) }
+
   # convert to an array
   nss<-dim(OUT)[1]
   n<-dim(Z)[1]/nss
   k<-dim(Z)[2]
   PZ<-array(dim=c(n,k,nss))
   for(i in 1:nss) { PZ[,,i]<-as.matrix(Z[ ((i-1)*n+1):(i*n) ,])  }
-  
+
   # burn and turn
   PZ<-PZ[,,-(1:round(nss/2))]
-  
+
   # mean latent positions
   ZTZ<-matrix(0,n,n)
   for(i in 1:dim(PZ)[3] ) { ZTZ<-ZTZ+PZ[,,i]%*%t(PZ[,,i]) }
-  ZTZ<-ZTZ/dim(PZ)[3] 
-  
+  ZTZ<-ZTZ/dim(PZ)[3]
+
   # a configuration that approximates posterior mean of ZTZ
   tmp<-eigen(ZTZ)
   Z.pm<-tmp$vec[,1:k]%*%sqrt(diag(tmp$val[1:k]))
-  
+
   # proc so we can plot with variance
   for(i in 1:dim(PZ)[3] ) { PZ[,,i]<-proc.rr(PZ[,,i],Z.pm) }
-  
+
   # colors for plotting
   r<-atan2(Z.pm[,2],Z.pm[,1])
   r<-r+abs(min(r))
@@ -53,7 +54,7 @@ latSpaceGen = function(var,bw=TRUE){
   g<-1-r
   b<-(Z.pm[,2]^2+Z.pm[,1]^2)
   b<-b/max(b)
-  
+
   # empty plot with dims
   par(mfrow=c(1,1))
   plot(
@@ -64,7 +65,7 @@ latSpaceGen = function(var,bw=TRUE){
     axes=FALSE
     )
   abline(h=0,lty=2);abline(v=0,lty=2)
-  
+
   # add points to show variance
   if(!bw){
     for(i in 1:n) { points( PZ[i,1,],PZ[i,2,],pch=46,col=rgb(r[i],g[i],b[i]) ) }
@@ -72,22 +73,58 @@ latSpaceGen = function(var,bw=TRUE){
   if(bw){
     for(i in 1:n) { points( PZ[i,1,],PZ[i,2,],pch=46,col='lightgrey' ) }
   }
-  
+
   # add in country abbreviations
   cntries = countrycode(
     rownames(mats[[var]]),
     'cown', 'iso3c'
   )
   cntries[is.na(cntries)] = ''
-  text(Z.pm[,1],Z.pm[,2], cntries, cex=.5)
+  text(Z.pm[,1],Z.pm[,2], cntries, cex=.75)
 }
 ################################################################
 
 ################################################################
 # run fn to get lat space plots
-for(v in names(mats)){
-  jpeg(paste0('floats/figure2_',v,'.jpg'))
-  latSpaceGen(v)
-  dev.off()
-}
+# for(v in names(mats)){
+  # jpeg(paste0('floats/figure2_',v,'.jpg'))
+  v = names(mats)[1]
+  latSpaceGen(v, bw=FALSE)
+  # dev.off()
+# }
 ################################################################
+
+# add in country abbreviations
+cntries = countrycode(
+  rownames(mats[[1]]),
+  'cown', 'iso3c'
+)
+cntries[is.na(cntries)] = ''
+
+####
+loadPkg('cshapes')
+cmap = wmap = cshp(date=as.Date('2001-1-1'))
+wmap = wmap[which(as.character(wmap$ISO1AL3) %in% cntries),]
+coords=coordinates(wmap)
+rownames(coords)=wmap$ISO1AL3
+coords=coords[cntries,]
+
+length(intersect(cntries, wmap$ISO1AL3))
+length(setdiff(cntries, wmap$ISO1AL3))
+
+
+# Create colors
+rlon = pi*coords[,1]/180
+rlat = pi*coords[,2]/180
+
+slon =  (rlon-min(rlon))/(max(rlon)-min(rlon))
+slat =  (rlat-min(rlat))/(max(rlat)-min(rlat))
+ccols = rgb( slon^2,slat^2,(1-sqrt(slon*slat))^2)
+
+# Generate legend map
+mapCol = ccols[match(cmap$ISO1AL3, cntries)]
+mapCol[is.na(mapCol)] = 'grey'
+fname=paste0(outPath, 'map.eps')
+# postscript(file=fname, width=8, height=4, horizontal=FALSE, onefile = FALSE, paper = "special")
+plot(cmap, col=ccols)
+# dev.off()
